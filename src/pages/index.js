0bbd27d5091762ffc257fb70219c28b74958440c
quarkly-dev <dev@quarkly.io>
2021-04-20 12:41:58 +0300

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Button } from "@quarkly/widgets";
+import { Theme, Link, Text, Button, Image, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { Override, Section } from "@quarkly/components";
@@ -16,6 +16,18 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Components.QuarklycommunityKitScrollIndicator />
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Button font="--lead" margin="20px">
+				Button
+			</Button>
+		</Section>
 		<Components.QuarklycommunityKitBackToTop>
 			<Section padding="100px 0" sm-padding="40px 0" background="#5463DE">
 				<Override slot="SectionContent" align-items="center" />
@@ -55,6 +67,25 @@ export default (() => {
 			</Components.QuarklycommunityKitCarousel>
 			<Components.QuarklycommunityKitYoomoneyDonateForm account="410015781814942" targets="for a cup of coffee" fio />
 		</Components.QuarklycommunityKitBackToTop>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+			</Text>
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503341960582-b45751874cf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503342394128-c104d54dba01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+		</Section>
 		<Components.QuarklycommunityKitLoopText />
 		<Link
 			font={"--capture"}
